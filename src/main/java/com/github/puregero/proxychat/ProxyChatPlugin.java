a54906f6d2fc5ed6c9244f148c1e8ed5415347a1
PureGero <puregero@gmail.com>
2022-06-15 22:47:56 +1000

    Add spectator chat

--- src/main/java/com/github/puregero/proxychat/ProxyChatPlugin.java
+++ src/main/java/com/github/puregero/proxychat/ProxyChatPlugin.java
@@ -2,6 +2,7 @@ package com.github.puregero.proxychat;
 
 import com.github.puregero.multilib.MultiLib;
 import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -13,7 +14,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class ProxyChatPlugin extends JavaPlugin implements Listener {
 
-    public int distance = 50;
+    public int distance = 100;
 
     @Override
     public void onEnable() {
@@ -22,18 +23,36 @@ public class ProxyChatPlugin extends JavaPlugin implements Listener {
         getServer().getPluginManager().registerEvents(this, this);
     }
 
+    private boolean canSeeAllChat(Player player) {
+        return "true".equals(MultiLib.getPersistentData(player, "proxychat.seeallchat"));
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onChatGhosts(AsyncPlayerChatEvent event) {
+        Player player = event.getPlayer();
+        if (player.getGameMode() == GameMode.SPECTATOR) {
+            String format = String.format(event.getFormat(), event.getPlayer().getName(), event.getMessage());
+            for (int i = 0; (i = format.indexOf('§', i + 1)) >= 0; ) {
+                format = format.substring(0, i) + ChatColor.GRAY + format.substring(i + 2);
+            }
+            event.setFormat(ChatColor.GRAY + format);
+
+            event.getRecipients().removeIf(recipient -> recipient.getGameMode() != GameMode.SPECTATOR && !canSeeAllChat(recipient));
+        }
+    }
+
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-    public void onChat(AsyncPlayerChatEvent event) {
+    public void onChatDistance(AsyncPlayerChatEvent event) {
         Player player = event.getPlayer();
         event.getRecipients().removeIf(recipient -> {
             boolean remove = recipient.getWorld() != player.getWorld() || horizontalDistanceSquared(recipient.getLocation(), player.getLocation()) > distance * distance;
 
-            if (remove && "true".equals(MultiLib.getPersistentData(recipient, "proxychat.seeallchat"))) {
+            if (remove && canSeeAllChat(recipient)) {
                 String format = String.format(event.getFormat(), event.getPlayer().getName(), event.getMessage());
                 for (int i = 0; (i = format.indexOf('§', i + 1)) >= 0; ) {
-                    format = format.substring(0, i) + ChatColor.GRAY + format.substring(i + 2);
+                    format = format.substring(0, i) + ChatColor.DARK_GRAY + format.substring(i + 2);
                 }
-                recipient.sendMessage(ChatColor.GRAY + format);
+                recipient.sendMessage(ChatColor.DARK_GRAY + format);
             }
 
             return remove;
